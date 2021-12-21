6810d3d937801c80e7c4068ca2c46f60daa640d7
Ryan Back <ryanpback@gmail.com>
2021-12-21 11:30:18 -0800

    try again

--- .github/scripts/open_pr.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-# puts ARGV[0]
-# puts ARGV[1]
-puts 'hello'
---
+++ .github/scripts/open_pr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
+  echo "This script requires the following args - in order"
+  echo -e "\t1.) Github API URL\n" \
+          "\t2.) Github Repository\n" \
+          "\t3.) Branch name for pull request into dmz"
+  exit 1
+fi
+
+GITHUB_API=$1
+REPO=$2
+BRANCH=$3
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  "${GITHUB_API}/repos/${REPO}/pulls" \
+  -d '{"head": "${BRANCH}", "base": "dmz"}'
